883cb932673f33c23f9a869a17b10cc9f0e4a0ab
Chintan Khetiya <khetiya.chintan@gmail.com>
2013-06-01 12:17:09 +0530

    first commit

--- EditText_Validation/gen/chintan/khetiya/android/EditText_Validation/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package chintan.khetiya.android.EditText_Validation;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
