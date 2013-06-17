f89e84b7ca6c00cc523a72d1adf7af69043561dc
Chintan Khetiya <khetiya.chintan@gmail.com>
2013-06-17 16:41:41 +0530

    fist comment

--- Custom_Dialog/gen/com/example/chintan/khetiya/android/custom_dialog/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.example.chintan.khetiya.android.custom_dialog;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
